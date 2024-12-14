a72d69cc5126af1945201c5245a903998ad1a294
Jonathan-Steel <j.p.l.steel@sms.ed.ac.uk>
2024-12-14 14:16:03 +0000

    Added worlds

--- src/managers/LevelManager.ts
+++ src/managers/LevelManager.ts
@@ -1,4 +1,5 @@
 import { Dialogue } from "./DialogueManager";
+import { LocalStorageManager } from "../managers/LocalStorageManager";
 
 export interface Level {
     id: number;
@@ -10,22 +11,82 @@ export interface Level {
     dialogue?: Dialogue;
 }
 
+export interface World {
+    id: number;
+    name: string;
+}
+
 export default class LevelManager {
     private levels: Level[];
+    private worlds: World[];
 
-    constructor(levels: Level[]) {
+    constructor(levels: Level[], worlds: World[]) {
         this.levels = levels;
+        this.worlds = worlds;
+    }
+
+    public get length(): number {
+        return this.levels.length;
+    }
+
+    public isNewPlayer(): boolean {
+        return localStorage.length === 0; // Returns true if no keys are present
+    }
+
+    public getLastUnlockedLevelID(): number {
+        for (let level of this.levels) {
+            if (!(this.loadGrade(level.id))) {
+                return level.id;
+            }
+        }
+        return this.levels.length - 1;
+    }
+
+    public getLastUnlockedWorldID(): number {
+        return this.loadLevel(this.getLastUnlockedLevelID()).world;
+    }
+
+    public getWorldLevelIDs(world: number): number[] {
+        return this.levels.filter((level) => level.world === world).map((level) => level.id);
     }
 
     public getAllLevels(): Level[] {
         return this.levels;
     }
 
-    public loadLevel(level: number): Level {
-        if (level < 1 || level > this.levels.length) {
+    public loadLevel(level_id: number): Level {
+        if (level_id < 0 || level_id >= this.levels.length ) {
             throw new Error("Invalid level");
         }
-        return this.levels[level - 1];
+        return this.levels[level_id];
+    }
+
+    public getWorld(world_id: number): World {  
+        if (world_id < 0 || world_id >= this.worlds.length) {
+            throw new Error("Invalid world");
+        }
+        return this.worlds[world_id];
+    }
+
+    public loadGrade(level_id: number): number {
+        let grade = LocalStorageManager.loadData<number>(level_id.toString());
+        if (grade === null) {
+            return 0;
+        }
+        return grade;
+    }
+
+    public getPreviousWorldID(world_id: number): number {
+        if (world_id <= 0) {
+            return -1;
+        }
+        return world_id - 1;
+    }
+
+    public getNextWorldID(world_id: number): number {
+        if (world_id >= this.worlds.length - 1) {
+            return -1;
+        }
+        return world_id + 1;
     }
 }
-    
--- src/scenes/LevelSelect.ts
+++ src/scenes/LevelSelect.ts
@@ -1,12 +1,16 @@
 import { Scene } from "phaser";
-import LevelManager, { Level } from "../managers/LevelManager";
-import { LocalStorageManager } from "../managers/LocalStorageManager";
+import LevelManager, { Level, World } from "../managers/LevelManager";
 import DialogueManager from "../managers/DialogueManager";
 
-
 export class LevelSelect extends Scene {
-    private level: number;
     private level_manager: LevelManager;
+    private current_world_id: number;
+
+    private world_text: Phaser.GameObjects.Text;
+    private level_buttons: Phaser.GameObjects.Text[] = [];
+    private stars: Phaser.GameObjects.Image[] = [];
+    private previousWorldButton: Phaser.GameObjects.Text;
+    private nextWorldButton: Phaser.GameObjects.Text;
 
     constructor() {
         super("LevelSelect");
@@ -14,13 +18,16 @@ export class LevelSelect extends Scene {
 
     preload() {
         this.load.json("levels", "data/levels.json");
+        this.load.json("worlds", "data/worlds.json");
     }
 
     create() {
         const levels: Level[] = this.cache.json.get("levels");
-        this.level_manager = new LevelManager(levels);
+        const worlds: World[] = this.cache.json.get("worlds");
+        this.level_manager = new LevelManager(levels, worlds);
+        this.current_world_id = 0;
 
-        if (this.isNewPlayer()) {
+        if (this.level_manager.isNewPlayer()) {
             let dialogue_manager = new DialogueManager(this);
             dialogue_manager.displayDialogue({
                 id: "game-intro",
@@ -32,28 +39,42 @@ export class LevelSelect extends Scene {
                 this.addLevelSelect();
             });
         } else {
+            let last_unlocked_world_id =
+                this.level_manager.getLastUnlockedWorldID();
+            this.current_world_id = last_unlocked_world_id;
             this.addLevelSelect();
         }
     }
 
-    private isNewPlayer(): boolean {
-        return localStorage.length === 0; // Returns true if no keys are present
-    }
-
     private addLevelSelect() {
-        this.add.text(100, 100, "Level Select", {
-            fontFamily: "WorkSansBold, Arial, sans-serif",
-            fontSize: 64,
-            color: "#000000",
-            align: "center",
-        });
-        this.addLevelButtons();
+        this.world_text = this.add.text(
+            100,
+            100,
+            `${this.level_manager.getWorld(this.current_world_id).name}`,
+            {
+                fontFamily: "WorkSansBold, Arial, sans-serif",
+                fontSize: 64,
+                color: "#000000",
+                align: "center",
+            }
+        );
+
+        this.addLevelButtons(
+            this.level_manager.getWorldLevelIDs(this.current_world_id)
+        );
+
+        this.addWorldButtons();
     }
 
-    private addLevelButton(x: number, y: number, level: Level, accessible: boolean = true) {
+    private addLevelButton(
+        x: number,
+        y: number,
+        level: Level,
+        accessible: boolean = true
+    ) {
         if (accessible) {
             let hoverTween: Phaser.Tweens.Tween | null = null;
-            const levelButton = this.add
+            const level_button = this.add
                 .text(x, y, `${level.name}`, {
                     fontFamily: "WorkSansBold, Arial, sans-serif",
                     fontSize: 32,
@@ -72,7 +93,7 @@ export class LevelSelect extends Scene {
                     }
 
                     hoverTween = this.add.tween({
-                        targets: levelButton,
+                        targets: level_button,
                         ease: "Sine.easeInOut",
                         duration: 100,
                         alpha: 0.5,
@@ -83,27 +104,27 @@ export class LevelSelect extends Scene {
                         hoverTween.stop();
                         hoverTween = null;
                     }
-                    levelButton.setAlpha(1);
+                    level_button.setAlpha(1);
                 });
+            this.level_buttons.push(level_button);
         } else {
-            this.add
-                .text(x, y, `${level.name}`, {
-                    fontFamily: "WorkSansBold, Arial, sans-serif",
-                    fontSize: 32,
-                    color: "#777777",
-                    align: "center",
-                });
+            const level_button = this.add.text(x, y, `${level.name}`, {
+                fontFamily: "WorkSansBold, Arial, sans-serif",
+                fontSize: 32,
+                color: "#777777",
+                align: "center",
+            });
+            this.level_buttons.push(level_button);
         }
-
     }
 
-    private addLevelButtons() {
-        let levels = this.level_manager.getAllLevels();
+    private addLevelButtons(level_ids: number[]) {
         let x = 100;
         let y = 200;
-        for (let level of levels) {
-            if (level.id != 1) {
-                if (this.loadGrade(level.id - 1)) {
+        for (let i of level_ids) {
+            const level = this.level_manager.loadLevel(i);
+            if (i != 0) {
+                if (this.level_manager.loadGrade(i - 1)) {
                     this.addLevelButton(x, y, level, true);
                 } else {
                     this.addLevelButton(x, y, level, false);
@@ -111,7 +132,11 @@ export class LevelSelect extends Scene {
             } else {
                 this.addLevelButton(x, y, level, true);
             }
-            this.addStars(this.scale.width - 100, y+15, this.loadGrade(level.id));
+            this.addStars(
+                this.scale.width - 100,
+                y + 15,
+                this.level_manager.loadGrade(i)
+            );
             y += 50;
         }
     }
@@ -123,9 +148,7 @@ export class LevelSelect extends Scene {
         let second_star = this.add
             .image(x - 60, y, "star")
             .setDisplaySize(40, 40);
-        let third_star = this.add
-            .image(x, y, "star")
-            .setDisplaySize(40, 40);
+        let third_star = this.add.image(x, y, "star").setDisplaySize(40, 40);
 
         if (grade === 0) {
             first_star.setTint(0x777777);
@@ -137,13 +160,124 @@ export class LevelSelect extends Scene {
         } else if (grade === 2) {
             third_star.setTint(0x777777);
         }
+
+        this.stars.push(first_star);
+        this.stars.push(second_star);
+        this.stars.push(third_star);
     }
 
-    private loadGrade(level_id: number): number {
-        let grade = LocalStorageManager.loadData<number>(level_id.toString());
-        if (grade === null) {
-            return 0;
+    private addWorldButtons() {
+        let previousWorldID = this.level_manager.getPreviousWorldID(
+            this.current_world_id
+        );
+        let nextWorldID = this.level_manager.getNextWorldID(
+            this.current_world_id
+        );
+        if (previousWorldID != -1) {
+            let hoverTween: Phaser.Tweens.Tween | null = null;
+            this.previousWorldButton = this.add
+                .text(
+                    10,
+                    this.scale.height - 40,
+                    `<- ${this.level_manager.getWorld(previousWorldID).name}`,
+                    {
+                        fontFamily: "WorkSansBold, Arial, sans-serif",
+                        fontSize: 32,
+                        color: "#000000",
+                        align: "center",
+                    }
+                )
+                .setInteractive()
+                .on("pointerdown", () => {
+                    this.current_world_id = previousWorldID;
+                    this.reload();
+                })
+                .on("pointerover", () => {
+                    // Stop any existing tweens
+                    if (hoverTween) {
+                        hoverTween.stop();
+                        hoverTween = null;
+                    }
+
+                    hoverTween = this.add.tween({
+                        targets: this.previousWorldButton,
+                        ease: "Sine.easeInOut",
+                        duration: 100,
+                        alpha: 0.5,
+                    });
+                })
+                .on("pointerout", () => {
+                    if (hoverTween) {
+                        hoverTween.stop();
+                        hoverTween = null;
+                    }
+                    this.previousWorldButton.setAlpha(1);
+                });
         }
-        return grade;
+
+        if (nextWorldID != -1 && nextWorldID <= this.level_manager.getLastUnlockedWorldID()) {
+            let hoverTween: Phaser.Tweens.Tween | null = null;
+            this.nextWorldButton = this.add
+                .text(
+                    this.scale.width - 10,
+                    this.scale.height - 40,
+                    `${this.level_manager.getWorld(nextWorldID).name} ->`,
+                    {
+                        fontFamily: "WorkSansBold, Arial, sans-serif",
+                        fontSize: 32,
+                        color: "#000000",
+                        align: "center",
+                    }
+                )
+                .setOrigin(1, 0)
+                .setInteractive()
+                .on("pointerdown", () => {
+                    this.current_world_id = nextWorldID;
+                    this.reload();
+                })
+                .on("pointerover", () => {
+                    // Stop any existing tweens
+                    if (hoverTween) {
+                        hoverTween.stop();
+                        hoverTween = null;
+                    }
+
+                    hoverTween = this.add.tween({
+                        targets: this.nextWorldButton,
+                        ease: "Sine.easeInOut",
+                        duration: 100,
+                        alpha: 0.5,
+                    });
+                })
+                .on("pointerout", () => {
+                    if (hoverTween) {
+                        hoverTween.stop();
+                        hoverTween = null;
+                    }
+                    this.nextWorldButton.setAlpha(1);
+                });
+        }
+    }
+
+    private reload() {
+        this.destroyLevelSelectPage();
+        this.addLevelSelect();
+    }
+
+    private destroyLevelSelectPage() {
+        this.world_text.destroy();
+
+        for (let button of this.level_buttons) {
+            button.destroy();
+        }
+        this.level_buttons = [];
+
+        for (let star of this.stars) {
+            star.destroy();
+        }
+        this.stars = [];
+
+        if (this.previousWorldButton) this.previousWorldButton.destroy();
+        if (this.nextWorldButton) this.nextWorldButton.destroy();
     }
 }
