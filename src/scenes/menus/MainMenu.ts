f7d2de14e73fdbe3b016a028cbb1988d83b13b50
Jonathan-PL-Steel <s2153660@ed.ac.uk>
2025-02-06 11:03:08 +0000

    Added Credits

--- src/scenes/menus/MainMenu.ts
+++ src/scenes/menus/MainMenu.ts
@@ -9,6 +9,7 @@ export class MainMenu extends Scene {
     title: GameObjects.Text;
     subtitle: GameObjects.Text;
     versionText: GameObjects.Text;
+    creditsText: GameObjects.Text;
 
     constructor() {
         super("MainMenu");
@@ -39,9 +40,27 @@ export class MainMenu extends Scene {
             color: "#000000",
         });
 
-        let edi_logo = this.add.image(10, this.scale.height - 10, "edi-logo").setOrigin(0, 1);
-        
-        edi_logo.setDisplaySize(edi_logo.displayWidth * 0.25, edi_logo.displayHeight * 0.25);
+        let edi_logo = this.add
+            .image(10, this.scale.height - 10, "edi-logo")
+            .setOrigin(0, 1);
+
+        edi_logo.setDisplaySize(
+            edi_logo.displayWidth * 0.25,
+            edi_logo.displayHeight * 0.25
+        );
+
+        this.creditsText = this.add.text(
+            this.scale.width - 10,
+            this.scale.height - 10,
+            "By Jonathan Steel (s2153660@ed.ac.uk)\n\nSupervised by Murray Cole (M.Cole@ed.ac.uk)",
+            {
+                fontFamily: "WorkSansRegular, Arial, sans-serif",
+                fontSize: 20,
+                color: "#000000",
+                wordWrap: { width: 500 },
+                align: "right",
+            }
+        ).setOrigin(1, 1);
 
         this.input.once("pointerdown", () => {
             this.sound.play("switch");
